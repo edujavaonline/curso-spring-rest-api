c800888af344d43239fb4ba4caf786fda915c7b7
eduardojavaonline <30944374+edujavaonline@users.noreply.github.com>
2021-10-09 18:30:34 -0300

    Add files via upload

---
+++ Aula 3 - Criação das tabelas do projeto do curso com Flyway/uniongroup-api/src/test/java/com/uniongroup/api/UniongroupApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniongroup.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniongroupApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
