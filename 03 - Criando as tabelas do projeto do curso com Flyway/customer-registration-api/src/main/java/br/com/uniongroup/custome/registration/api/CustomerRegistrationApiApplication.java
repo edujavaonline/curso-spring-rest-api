72ec0bb25006743c2d77388644872794ee99952d
eduardojavaonline <30944374+edujavaonline@users.noreply.github.com>
2022-01-12 13:07:20 -0300

    Criando as tabelas do projeto do curso com Flyway

---
+++ 03 - Criando as tabelas do projeto do curso com Flyway/customer-registration-api/src/main/java/br/com/uniongroup/custome/registration/api/CustomerRegistrationApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.uniongroup.custome.registration.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerRegistrationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerRegistrationApiApplication.class, args);
+	}
+
+}
