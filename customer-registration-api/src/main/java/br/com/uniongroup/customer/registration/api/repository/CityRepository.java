ff5fcdf5307a9b51069a93440e043086032ee2d1
eduardojavaonline <30944374+edujavaonline@users.noreply.github.com>
2023-07-19 19:51:17 -0300

    Delete customer-registration-api directory

--- customer-registration-api/src/main/java/br/com/uniongroup/customer/registration/api/repository/CityRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.uniongroup.customer.registration.api.repository;
-
-import br.com.uniongroup.customer.registration.api.model.City;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CityRepository extends JpaRepository<City, Long> {
-}
--- customer-registration-api/src/main/java/br/com/uniongroup/customer/registration/api/repository/CustomerRegisterRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.uniongroup.customer.registration.api.repository;
-
-import br.com.uniongroup.customer.registration.api.model.CustomerRegister;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRegisterRepository extends JpaRepository<CustomerRegister, Long> {
-}
